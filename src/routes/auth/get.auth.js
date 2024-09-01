0d413f12e2d2b1671c4956e1d472b226f1b60d4d
matt flores <floresmatias0@gmail.com>
2024-09-01 14:37:26 -0300

    try-catch in auth routes added

--- src/routes/auth/get.auth.js
+++ src/routes/auth/get.auth.js
@@ -8,59 +8,86 @@ const calendarScopes = [
 
 // Middleware para definir el scope según el rol
 function defineGoogleScope(req, res, next) {
-    const { role } = req.query;
+    try {
+        const { role } = req.query;
 
-    const baseScope = ['email', 'profile'];
-    let scope;
+        const baseScope = ['email', 'profile'];
+        let scope;
 
-    if (role === "PACIENTE") {
-        scope = baseScope; // Pacientes no necesitan acceso al calendario
-    } else {
-        scope = [...baseScope, ...calendarScopes]; // Otros roles sí necesitan acceso al calendario
+        if (role === "PACIENTE") {
+            scope = baseScope; // Pacientes no necesitan acceso al calendario
+        } else {
+            scope = [...baseScope, ...calendarScopes]; // Otros roles sí necesitan acceso al calendario
+        }
+
+        req.googleScope = scope;
+        next();
+    } catch (error) {
+        next(error); // Pasa el error al middleware de manejo de errores de Express
     }
-
-    req.googleScope = scope;
-    next();
 }
+
 
 // Ruta para iniciar la autenticación con Google
 server.get('/google/', defineGoogleScope, (req, res, next) => {
-    passport.authenticate('google', {
-        scope: req.googleScope,
-        accessType: 'offline',
-        prompt: 'consent',
-        state: req.query.role,
-        passReqToCallback: true
-    })(req, res, next);
-});
-
-server.get('/google/callback',
-    passport.authenticate('google', {
-        successRedirect: '/v1/auth/google/success',
-        failureRedirect: '/v1/auth/google/failure'
+    try {
+        passport.authenticate('google', {
+            scope: req.googleScope,
+            accessType: 'offline',
+            prompt: 'consent',
+            state: req.query.role,
+            passReqToCallback: true
+        })(req, res, next);
+    } catch (error) {
+        next(error); // Maneja el error
     }
-));
-
-server.get('/google/success', (req, res) => {
-    const { user } = req;
-    res.redirect(process.env.FRONTEND_URL + '/verificacion?_valid=' + user?._id);
 });
 
-server.get('/google/failure', (req, res) => {
-    res.redirect(process.env.FRONTEND_URL);
+server.get('/google/callback', (req, res, next) => {
+    try {
+        passport.authenticate('google', {
+            successRedirect: '/v1/auth/google/success',
+            failureRedirect: '/v1/auth/google/failure'
+        })(req, res, next);
+    } catch (error) {
+        next(error); // Maneja el error
+    }
 });
 
-server.get('/google/logout', (req, res) => {
-    req.session.destroy((err) => {
-        if (err) {
-            console.log('Error al destruir la sesión:', err);
-            throw new Error("No se pudo destruir la session")
+server.get('/google/success', (req, res, next) => {
+    try {
+        const { user } = req;
+        if (!user) {
+            throw new Error('No user found after authentication');
         }
-        res.clearCookie('connect.sid', {
-            path: '/'
+        res.redirect(process.env.FRONTEND_URL + '/verificacion?_valid=' + user?._id);
+    } catch (error) {
+        next(error); // Maneja el error
+    }
+});
+
+server.get('/google/failure', (req, res, next) => {
+    try {
+        res.redirect(process.env.FRONTEND_URL);
+    } catch (error) {
+        next(error); // Maneja el error
+    }
+});
+
+
+server.get('/google/logout', (req, res, next) => {
+    try {
+        req.session.destroy((err) => {
+            if (err) {
+                console.log('Error al destruir la sesión:', err);
+                throw new Error("No se pudo destruir la sesión");
+            }
+            res.clearCookie('connect.sid', { path: '/' });
+            res.redirect(`${process.env.FRONTEND_URL}/iniciar-sesion`); // Redirigir después de cerrar sesión
         });
-        res.redirect(`${process.env.FRONTEND_URL}/iniciar-sesion`); // Redirigir después de cerrar sesión
-    });
+    } catch (error) {
+        next(error); // Maneja el error
+    }
 });
 
 module.exports = server;
