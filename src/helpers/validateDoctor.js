f2824b21a0e999d9531db47e5e90e2484a5174af
Sebastián Ariel Commidari <sebastian.commidari@gmail.com>
2024-12-10 17:54:48 -0300

    Prueba arreglo de Validación de Médicos

--- src/helpers/validateDoctor.js
+++ src/helpers/validateDoctor.js
@@ -20,6 +20,7 @@ const createMessageEvaluationEmail = (result) => {
   }
 }
 
+
 const validateDoctorAndUpdateDB = async(id, dni, firstName, lastName, email) => {
   try {
     const response = await validateDoctor(dni)
@@ -27,96 +28,153 @@ const validateDoctorAndUpdateDB = async(id, dni, firstName, lastName, email) => 
     let data = {validated:'disabled'}
     if(response.matricula) data.validated = 'completed'
     await updateUser(id, data);
-    const emailMessage = createMessageEvaluationEmail(createMessageEvaluationEmail.validated)
-    try {
-      const emailService = await doctorEvaluationEmail(email, `${lastName} ${firstName}`, emailMessage)
-    }catch(err) {
-      console.log(err)
-    }
+    //const emailMessage = createMessageEvaluationEmail(createMessageEvaluationEmail.validated)
+    //try {
+      //const emailService = await doctorEvaluationEmail(email, ${lastName} ${firstName}, emailMessage)
+    //}catch(err) {
+     // console.log(err)
+    //}
   }catch(err){
-    console.log(err)
+   console.log(err)
   }
 }
 
+
 const validateDoctor = async (document) => {
-  const documento = String(document);
-  console.log('Documento:', documento);
-  
-  let browser;
+  const documento = String(document)
+  console.log(documento)
   try {
-    browser = await puppeteer.launch({
+    const browser = await puppeteer.launch({
       headless: true,
       args: ['--no-sandbox', '--disable-setuid-sandbox'],
+      protocolTimeout: 60000
+    });
+    console.log('browser iniciado')
+    const page = await browser.newPage()
+    await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3');
+    page.setDefaultTimeout(600000)
+    console.log(sisaUrl)
+    await page.goto(sisaUrl, {
+      waitUntil: 'networkidle0'
     });
 
-    const page = await browser.newPage();
-    await page.setUserAgent(
-      'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
-    );
-    page.setDefaultTimeout(600000);
-
-    console.log('Navegando a:', sisaUrl);
-    await page.goto(sisaUrl, { waitUntil: 'networkidle0' });
-
+    const cookies = await page.cookies();
+    await page.setCookie(...cookies);
     await page.waitForSelector(selectorRegistro);
-    console.log('Botón encontrado, haciendo clic');
-    await page.click(selectorRegistro);
-
-    await page.click(selectorTipo);
-    await page.type(imput, documento);
-    await page.click(searchButton);
+    console.log('encontro el boton')
+    await page.click(selectorRegistro)
+    console.log('hizo click en el menu')
+    await page.click(selectorTipo)
+    await page.locator(imput).fill(documento)
+    await page.click(searchButton)
 
     try {
-      await page.waitForSelector(firstResult, { timeout: 3000 });
+      try {
+        await page.waitForSelector(firstResult, {
+          timeout: 3000
+        })
+      }catch(err) {
+        //aca si no existe deberia retomar que no es médico, definir respuesta
+        await browser.close();
+        console.log('no es medico')
+        if(err.name === 'TimeoutError') return false
+      }
+      await page.click(firstResult)
+      await page.waitForSelector(resultTitle, {
+        timeout: 5000
+      })
+      // const title = await page.evaluate(() => {
+      //   const nombre = document.querySelector(resultTitle)
+      //   return nombre.innerText
+      // })
+      const title = await page.evaluate((resultTitle) => {
+        const nombre = document.querySelector(resultTitle);
+        return nombre ? nombre.innerText : null; // Verifica que el elemento existe
+      }, resultTitle); // Pasa la variable resultTitle al contexto del navegador
+
+      const codProf = await page.evaluate((codProfInfo) => {
+        const codigo = document.querySelector(codProfInfo)
+        return codigo? codigo.innerText: null
+      },codProfInfo)
+      const data1 = await page.evaluate((tableInfo1) => {
+        const table1 = document.querySelector(tableInfo1)
+        const rows1 = table1.querySelectorAll('tbody tr');
+        return Array.from(rows1, row => {
+          const columns = row.querySelectorAll('td');
+          return Array.from(columns).map(td => td.innerText.trim());
+        });
+      }, tableInfo1)
+      const data2 = await page.evaluate((tableInfo2) => {
+        const table2 = document.querySelector(tableInfo2)
+        const rows2 = table2.querySelectorAll('tbody tr');
+        return Array.from(rows2, row => {
+          const columns = row.querySelectorAll('td div');
+          return Array.from(columns, column => column.innerText);
+        });
+      },tableInfo2)
+      console.log(codProf)
+      console.log(data1)
+      console.log(data2)
+      const regex = /Ficha personal de (.+), DNI (\d+)/;
+      let nombre = null
+      let dni = null
+      const match = title.match(regex);
+      if (match) {
+        nombre = match[1].trim();
+        dni = match[2];
+      }
+      console.log(data1[0])
+      const valuesWithNewLines = data1[0].filter(value => value.includes('\n'));
+      console.log(valuesWithNewLines)
+      
+      const parsedData = valuesWithNewLines.map(item => {
+        const regex = /^(.*)\n{3}Matrícula\s(.+?)\n{3}(?:Especialista\. en: (.+?)\n{3})?Habilitado en (.+)$/;
+        const regex2 = /^(.*)\n{3}Matrícula\s(.+?)\n{5}Habilitado en (.+)$/;
+        const match = item.match(regex);
+        if (match) {
+          return {
+            especialidad: match[1].trim(),
+            matricula: match[2].trim(),
+            especialista: match[3] ? match[3].trim() : null,
+            habilitadoEn: match[4].trim()
+          };
+        }
+        const match2 = item.match(regex2);
+        if (match2) {
+          return {
+            especialidad: match2[1].trim(),
+            matricula: match2[2].trim(),
+            habilitadoEn: match2[3].trim()
+          };
+        }
+        return null;
+      });
+
+
+      console.log(parsedData)
+      const matricula = parsedData.filter(item => item !== null);
+      const otrosDatos = [...new Set(data2.flat().filter(item => item !== ''))];
+
+      let formatResponse = {
+        nombre,
+        dni,
+        codigoProfesional: codProf,
+        matricula,
+        otrosDatos
+      }
+      console.log(formatResponse)
+      await browser.close();
+      return formatResponse
     } catch (err) {
-      console.log('No es médico');
-      return false;
+      await browser.close();
+      console.log(err)
+      return false
     }
-
-    await page.click(firstResult);
-    await page.waitForSelector(resultTitle);
-
-    const title = await page.evaluate((sel) => {
-      const element = document.querySelector(sel);
-      return element ? element.innerText : null;
-    }, resultTitle);
-
-    const codProf = await page.evaluate((sel) => {
-      const element = document.querySelector(sel);
-      return element ? element.innerText : null;
-    }, codProfInfo);
-
-    const data1 = await extractTableData(page, tableInfo1);
-    const data2 = await extractTableData(page, tableInfo2, true);
-
-    console.log('Datos obtenidos:', { title, codProf, data1, data2 });
-
-    const regex = /Ficha personal de (.+), DNI (\d+)/;
-    const match = title.match(regex);
-    const nombre = match ? match[1].trim() : null;
-    const dni = match ? match[2] : null;
-
-    const matricula = parseData(data1); // Mueve la lógica de regex a una función separada
-    const otrosDatos = [...new Set(data2.flat().filter((item) => item !== ''))];
-
-    const response = {
-      nombre,
-      dni,
-      codigoProfesional: codProf,
-      matricula,
-      otrosDatos,
-    };
-
-    console.log('Respuesta formateada:', response);
-    return response;
   } catch (err) {
-    console.error('Error:', err);
-    return false;
-  } finally {
-    if (browser) await browser.close();
+    console.log(err)
+    return err
   }
-};
-
+}
 
 
 
